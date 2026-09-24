edb13e9083c2542269ab6b9c295dfe5c1764cbd5
h3cknn <real.heckerrr@gmail.com>
2026-09-24 17:50:29 +0300

    Correct lpmake wrapper binary path

--- scripts/lpmake_wrapper.sh
+++ scripts/lpmake_wrapper.sh
@@ -3,4 +3,4 @@
 set -Eeuo pipefail
 
 export LD_LIBRARY_PATH="/usr/local/lib/h3cknn-gsi/aosp-lib64:/usr/lib/x86_64-linux-gnu/android${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-exec /usr/local/lib/h3cknn-gsi/lpmake.bin "$@"
+exec /usr/local/lib/h3cknn-gsi/aosp-lib64/lpmake.bin "$@"
